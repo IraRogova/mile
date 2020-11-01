3318b8afa62ef2c691482baa11c928eb55b8a2f5
Irina Rogova <buyersbirds@gmail.com>
2020-11-01 14:17:30 +0600

    Initinal commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 35000;
+        int mile = 20 * 1;
+        int TotalMile = ticket / mile;
+        System.out.println(TotalMile);
+    }
+}
